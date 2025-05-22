24dc47c199aa5693e5ed531749fc42b4f366c494
Himanshu-1150 <parjapatihim1150@gmail.com>
2025-05-22 17:19:58 +0530

    give styling and change layout of the website using Bootstrap

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const ejsMate = require("ejs-mate");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 
 const MongoUrl = "mongodb://127.0.0.1:27017/wanderlust";
 
